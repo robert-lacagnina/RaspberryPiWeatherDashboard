a24a75f90aee1cbf5b3e26f7140b85cdfd70a09c
Rob LaCagnina <robert.lacagnina@gmail.com>
2014-03-27 18:55:44 -0400

    got client to communicate with sever using socket.io

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+//allow socket.io to be served to the client
+/*app.use(express.static(__dirname + '/socket.io/'));*/
+
+
 //GET: / - load the main dashboad page
 app.get('/', function(req, res){
   res.sendfile('./nodetest.htm');
@@ -17,7 +21,13 @@ var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
-//sockets io using express web server
+//socket io using express web server
 var io = require('socket.io').listen(server);
 
-
+io.sockets.on('connection', function (socket) {
+	//when the learnmore event is fired emit the message back to the client
+	socket.on('learnmore', function () {
+		socket.emit('message', { message : 'thanks for learning more!' });
+		console.log('emmitteeded a message');
+	});
+});
